4909bf470e3e118eb8882e5fe346a3a234337b4b
Serafimklm <joaopixi48050e@gmail.com>
2025-05-28 21:05:45 -0300

    lagacy view

--- app/src/main/java/com/example/funcoescomoparametros/teste/MainActivity1.kt
+++ app/src/main/java/com/example/funcoescomoparametros/teste/MainActivity1.kt
@@ -2,7 +2,6 @@ package com.example.funcoescomoparametros.teste
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
@@ -23,7 +22,7 @@ class MainActivity1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main1)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.list_users)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
